35e0cf8a4c26015ef675a4e589f5cd7f3cd56962
Rsja12 <asocarras10@gmail.com>
2019-10-31 15:11:11 -0400

    adds '/accounts/new' route. Adds deposit and withdrawal links in '/accounts/:id'.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,7 +5,9 @@ class AccountsController < ApplicationController
         erb :"/accounts/index"
     end
 
-   
+   get '/accounts/new' do 
+        erb :"/accounts/new"
+   end
 
     get '/accounts/:id' do 
         @account = Account.find_by_id(params[:id])
