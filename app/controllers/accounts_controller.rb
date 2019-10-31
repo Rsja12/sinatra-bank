6ffb5d657678d9ff36b2c8792a196d8950dd1fe5
Rsja12 <asocarras10@gmail.com>
2019-10-31 16:34:52 -0400

    Adds edit route in accountscontroller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,19 +1,30 @@
 class AccountsController < ApplicationController
     
+    # Read
     get '/accounts' do 
         @accounts = Account.all 
         erb :"/accounts/index"
     end
 
+    # Create
    get '/accounts/new' do 
         erb :"/accounts/new"
    end
 
+    #Edit
+   get '/accounts/:id' do 
+    @accounts = Account.all
+    @account = Account.find_by_id(params[:id])
+    erb :"/accounts/edit"
+   end
+
+#    Read
     get '/accounts/:id' do 
         @account = Account.find_by_id(params[:id])
         erb :"/accounts/show"
     end
 
+    # Create
     post '/accounts' do
         account = Account.create(params)
         if account
@@ -22,5 +33,7 @@ class AccountsController < ApplicationController
             redirect to "/accounts/new"
         end
     end
+
+
 
 end
