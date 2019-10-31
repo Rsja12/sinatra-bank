c3200d2113aab51ed05462f3eb7a51eea45de525
Rsja12 <asocarras10@gmail.com>
2019-10-31 13:06:28 -0400

    Adds /accounts/:id route and /accounts/show page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,4 +5,11 @@ class AccountsController < ApplicationController
         erb :"/accounts/index"
     end
 
+   
+
+    get '/accounts/:id' do 
+        @account = Account.find_by_id(params[:id])
+        erb :"/accounts/show"
+    end
+
 end
