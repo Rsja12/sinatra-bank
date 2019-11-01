4962314c7f4107755c5d10d9aa9b84b78b0871af
Rsja12 <asocarras10@gmail.com>
2019-11-01 11:34:55 -0400

    Checks that Client name and email aren't empty

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,6 @@ class Client < ActiveRecord::Base
     has_many :accounts
     has_secure_password
     validates :email, uniqueness: true
+    validates :email, presence: true
+    validates :name, presence: true
 end
